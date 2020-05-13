7aa8b43600fc85688ff3350ee73d6739e319a057
mjmercado <mercado.mj2016@gmail.com>
2020-05-13 20:34:50 +0800

    First commit

---
+++ config.js
@@ -0,0 +1,11 @@
+export default {
+  shopifyDomain: "mjmdev-01.myshopify.com",
+  shopifyToken: "5750ee52d1d8a6271bae9c677002e012",
+  collectionHandle: null,
+  localStorageKey: "vue-shopify",
+  showUnavailableProducts: true,
+  productListColumns: 3,
+  productListColumnsMobile: 1,
+  loadingColor: "#41b883",
+  googleAnalyticsId: null
+};
